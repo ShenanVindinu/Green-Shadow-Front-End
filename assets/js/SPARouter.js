200307c6995324b17c74494db6fa7e6e039884e3
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-21 23:56:58 +0530

    new sidebar added with SPA features

--- assets/js/SPARouter.js
+++ assets/js/SPARouter.js
@@ -1,3 +1,27 @@
-$("#loginPage").css({display: 'none'});
-$("#signupPage").css({display: 'block'});
+$("#loginPage").css({display: 'block'});
+$("#signupPage").css({display: 'none'});
+
+
+
+$("#signupBtn").eq(0).on('click', () => {
+    $("#loginPage").css({display: 'none'});
+    $("#signupPage").css({display: 'block'});
+});
+
+$("#SignupBack").eq(0).on('click', () => {
+    $("#loginPage").css({display: 'block'});
+    $("#signupPage").css({display: 'none'});
+});
+
+$("#staffBtn").eq(0).on('click', () => {
+    $("#dashboard-section").css({display: 'none'});
+    $("#staff-section").css({display: 'block'});
+});
+
+$("#dashboardBtn").eq(0).on('click', () => {
+    $("#dashboard-section").css({display: 'block'});
+    $("#staff-section").css({display: 'none'});
+});
+
+
 
