7e0b5da81f6b512f3b4d4795da59a02375acd4ac
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-22 00:00:18 +0530

    SPARouterDashboard.js created

---
+++ assets/js/SPARouterDashboard.js
@@ -0,0 +1,12 @@
+$("#staffBtn").eq(0).on('click', () => {
+    $("#dashboard-section").css({display: 'none'});
+    $("#staff-section").css({display: 'block'});
+});
+
+$("#dashboardBtn").eq(0).on('click', () => {
+    $("#dashboard-section").css({display: 'block'});
+    $("#staff-section").css({display: 'none'});
+});
+
+
+
--- assets/js/SPARouterLogging.js
+++ assets/js/SPARouterLogging.js
@@ -13,15 +13,5 @@ $("#SignupBack").eq(0).on('click', () => {
     $("#signupPage").css({display: 'none'});
 });
 
-$("#staffBtn").eq(0).on('click', () => {
-    $("#dashboard-section").css({display: 'none'});
-    $("#staff-section").css({display: 'block'});
-});
-
-$("#dashboardBtn").eq(0).on('click', () => {
-    $("#dashboard-section").css({display: 'block'});
-    $("#staff-section").css({display: 'none'});
-});
-
 
 
