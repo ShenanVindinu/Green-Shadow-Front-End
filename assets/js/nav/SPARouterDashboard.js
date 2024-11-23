52b4052df210a96e24c9d7bc1513be74e90ba48c
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-23 22:08:05 +0530

    nav created for Field section

--- assets/js/nav/SPARouterDashboard.js
+++ assets/js/nav/SPARouterDashboard.js
@@ -2,22 +2,29 @@ $("#dashboardSection").css({display: 'block'});
 $("#staffSection").css({display: 'none'});
 $("#vehicleSection").css({display: 'none'});
 $("#equipmentSection").css({display: 'none'});
+$("#FieldSection").css({display: 'none'});
 
 $("#staffBtn").eq(0).on('click', () => {
     $("#dashboardSection").css({display: 'none'});
     $("#vehicleSection").css({display: 'none'});
+    $("#equipmentSection").css({display: 'none'});
+    $("#FieldSection").css({display: 'none'});
     $("#staffSection").css({display: 'block'});
 });
 
 $("#dashboardBtn").eq(0).on('click', () => {
     $("#staffSection").css({display: 'none'});
     $("#vehicleSection").css({display: 'none'});
+    $("#equipmentSection").css({display: 'none'});
+    $("#FieldSection").css({display: 'none'});
     $("#dashboardSection").css({display: 'block'});
 });
 
 $("#vehicleBtn").eq(0).on('click', () => {
     $("#staffSection").css({display: 'none'});
     $("#dashboardSection").css({display: 'none'});
+    $("#equipmentSection").css({display: 'none'});
+    $("#FieldSection").css({display: 'none'});
     $("#vehicleSection").css({display: 'block'});
 })
 
@@ -25,8 +32,19 @@ $("#equipmentBtn").eq(0).on('click', () => {
     $("#staffSection").css({display: 'none'});
     $("#dashboardSection").css({display: 'none'});
     $("#vehicleSection").css({display: 'none'});
+    $("#FieldSection").css({display: 'none'});
     $("#equipmentSection").css({display: 'block'});
 })
+
+$("#fieldBtn").eq(0).on('click', () => {
+    $("#staffSection").css({display: 'none'});
+    $("#dashboardSection").css({display: 'none'});
+    $("#vehicleSection").css({display: 'none'});
+    $("#equipmentSection").css({display: 'none'});
+    $("#FieldSection").css({display: 'block'});
+})
+
+
 
 
 
