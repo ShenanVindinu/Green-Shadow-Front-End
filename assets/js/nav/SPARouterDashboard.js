a894e7b2805c1f353835cb9c3aab11aa08c66640
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-23 16:14:36 +0530

    vehicle section navigation added to SPARouterDashboard.js

--- assets/js/nav/SPARouterDashboard.js
+++ assets/js/nav/SPARouterDashboard.js
@@ -1,14 +1,24 @@
+$("#dashboardSection").css({display: 'block'});
 $("#staffSection").css({display: 'none'});
+$("#vehicleSection").css({display: 'none'});
 
 $("#staffBtn").eq(0).on('click', () => {
     $("#dashboardSection").css({display: 'none'});
+    $("#vehicleSection").css({display: 'none'});
     $("#staffSection").css({display: 'block'});
 });
 
 $("#dashboardBtn").eq(0).on('click', () => {
-    $("#dashboardSection").css({display: 'block'});
     $("#staffSection").css({display: 'none'});
+    $("#vehicleSection").css({display: 'none'});
+    $("#dashboardSection").css({display: 'block'});
 });
+
+$("#vehicleBtn").eq(0).on('click', () => {
+    $("#staffSection").css({display: 'none'});
+    $("#dashboardSection").css({display: 'none'});
+    $("#vehicleSection").css({display: 'block'});
+})
 
 
 
